8eef18d244bfdf8f2499c4554c1be218bb1451b0
keithchasen <keithchasen89@gmail.com>
2021-03-16 01:09:17 +0200

    using lodash for fetching books via graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+//dummy data
+var books = [
+  { name: 'Game of Throne', genre: 'fantasy', id: '1'},
+  { name: 'Clash of Kings', genre: 'fantasy', id: '2'},
+  { name: 'Three body problem', genre: 'sci-fi', id: '3'},
+  { name: 'Storm of Swords', genre: 'fantasy', id: '4'},
+];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -25,6 +34,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString }},
       resolve(parent, args) {
         // code to get data from DB / other source
+        return _.find(books, { id: args.id})
       }
     }
   }
