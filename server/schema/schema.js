8297b731fd03bfad04954916d96f037ea76de4ac
keithchasen <keithchasen89@gmail.com>
2021-03-16 20:30:17 +0200

    books list query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -65,6 +65,12 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         return _.find(authors, { id: args.id })
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return books;
+      }
     }
   }
 });
