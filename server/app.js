1426f22ee96171b5b9f868671335f7911ca11d25
keithchasen <keithchasen89@gmail.com>
2021-03-16 22:11:36 +0200

    mongoose and connecting to mongo db

--- server/app.js
+++ server/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+mongoose.connect(config.mongo, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}, error => {
+  if (error) {
+    console.log(error, 'Mongo error')
+  } else {
+    console.log('Connected to MongoDB')
+  }
+});
 
 const app = express();
 
---
+++ server/config.example.js
@@ -0,0 +1,2 @@
+const mongo = "";
+module.exports = { mongo };
