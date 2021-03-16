e9ac300e863b3d8da69ebf77260295dc10d7935e
keithchasen <keithchasen89@gmail.com>
2021-03-16 20:23:39 +0200

    list of books for authors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,7 +8,8 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -31,7 +32,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id:    { type: GraphQLID },
     name:  { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id})
+      }
+    }
   })
 });
 
