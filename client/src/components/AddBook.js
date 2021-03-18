0fe88f622195b4945521119a75728a8cd4fa65b1
keithchasen <keithchasen89@gmail.com>
2021-03-18 23:10:19 +0200

    using the state for collecting new book data

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { getAuthorsQuery } from "../queries/queries";
 
@@ -12,19 +13,28 @@ function AddBook() {
     )
   });
 
+  const [name, setName] = useState('');
+  const [genre, setGenre] = useState('');
+  const [authorId, setAuthorId] = useState('');
+
+  const submitForm = e => {
+    e.preventDefault();
+    console.log(name, genre, authorId)
+  };
+
   return (
-    <form id='add-book'>
+    <form id='add-book' onSubmit={(e) => submitForm(e)}>
       <div className='field'>
         <label>Book name:</label>
-        <input type="text"/>
+        <input type="text" onChange={(e) => setName(e.target.value)}/>
       </div>
       <div className='field'>
         <label>Genre:</label>
-        <input type="text"/>
+        <input type="text" onChange={(e) => setGenre(e.target.value)}/>
       </div>
       <div className='field'>
         <label>Author:</label>
-        <select>
+        <select onChange={(e) => setAuthorId(e.target.value)}>
           {content}
         </select>
       </div>
