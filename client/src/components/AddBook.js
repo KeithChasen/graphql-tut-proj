76ed5434dd942adddce14acade9753326d56f4a9
keithchasen <keithchasen89@gmail.com>
2021-03-18 09:56:42 +0200

    add book component / loading authors to select

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,45 @@
+import { gql, useQuery } from '@apollo/client';
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            id
+            name
+        }
+    }
+
+`;
+
+function AddBook() {
+  const { loading, error, data } = useQuery(getAuthorsQuery);
+  let content = null;
+  if (loading) content = <option>Loading...</option>;
+  else if (error) content = <option>Something went wrong :(</option>;
+  else if (data) content = data.authors.map(author => {
+    return (
+      <option key={author.id} value={author.id}>{ author.name }</option>
+    )
+  });
+
+  return (
+    <form id='add-book'>
+      <div className='field'>
+        <label>Book name:</label>
+        <input type="text"/>
+      </div>
+      <div className='field'>
+        <label>Genre:</label>
+        <input type="text"/>
+      </div>
+      <div className='field'>
+        <label>Author:</label>
+        <select>
+          {content}
+        </select>
+      </div>
+      <button>+</button>
+    </form>
+  );
+}
+
+export default AddBook;
